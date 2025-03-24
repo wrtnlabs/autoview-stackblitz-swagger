464f932f512a3819b11b7fa662a49a3e4b525a4a
AcrylicShrimp <led789zxpp@naver.com>
2025-03-24 16:46:06 +0900

    feat

--- vite.config.ts
+++ vite.config.ts
@@ -1,8 +1,9 @@
 import typia from "@ryoppippi/unplugin-typia/vite";
+import tailwindcss from '@tailwindcss/vite'
 import react from "@vitejs/plugin-react";
 import { defineConfig } from "vite";
 
 // https://vite.dev/config/
 export default defineConfig({
-  plugins: [typia(), react()],
+  plugins: [typia(), react(), tailwindcss()],
 });
