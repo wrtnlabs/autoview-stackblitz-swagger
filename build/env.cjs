4fcc89dc447463228e5fc50877b37f9ceb2be9f3
AcrylicShrimp <led789zxpp@naver.com>
2025-03-24 15:10:35 +0900

    feat

--- build/env.cjs
+++
@@ -1,4 +0,0 @@
-const fs = require("fs");
-
-if (fs.existsSync(`${__dirname}/../.env`) === false)
-  fs.copyFileSync(`${__dirname}/../.env.local`, `${__dirname}/../.env`);
--- src/generate.ts
+++ src/generate.ts
@@ -2,6 +2,7 @@ import { MainAgent } from "@autoview/agent";
 import dotenv from "dotenv";
 import fs from "fs";
 import OpenAI from "openai";
+import { exit } from "process";
 import typia from "typia";
 
 import { YourSchema } from "./YourSchema";
@@ -9,6 +10,8 @@ import { YourSchema } from "./YourSchema";
 dotenv.config();
 
 const main = async (): Promise<void> => {
+  checkApiKey();
+
   const result: MainAgent.IResult = await MainAgent.execute(
     {
       type: "chatgpt",
@@ -25,4 +28,20 @@ const main = async (): Promise<void> => {
     "utf8",
   );
 };
+
 main().catch(console.error);
+
+function checkApiKey(): void {
+  const apiKey = process.env.OPENAI_API_KEY;
+
+  if (!apiKey || apiKey === "YOUR_OPENAI_API_KEY") {
+    console.error("");
+    console.error("[========= API KEY ERROR =========]");
+    console.error("");
+    console.error("`OPENAI_API_KEY` is not set; please open the `.env` file and feed your own key");
+    console.error("");
+    console.error("[=================================]");
+    console.error("");
+    exit(-1);
+  }
+}
