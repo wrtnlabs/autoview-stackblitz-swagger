a3ecfaf65f974a963db47584fa22c6cc5d41a2ef
AcrylicShrimp <led789zxpp@naver.com>
2025-03-24 16:51:33 +0900

    fix

--- src/index.tsx
+++ src/index.tsx
@@ -1,4 +1,3 @@
-
 import { IAutoViewComponentProps } from "@autoview/interface";
 import { renderComponent } from "@autoview/ui";
 import { createRoot } from "react-dom/client";
@@ -7,33 +6,39 @@ import typia from "typia";
 import { YourSchema } from "./YourSchema";
 import { transform } from "./transform";
 
-import "./index.css";
-
 function Application() {
-  return <div className="max-w-[512px] mx-auto px-4 py-8">
-    <h1 className="text-2xl font-bold text-center">AutoView Playground</h1>
+  return (
+    <div className="max-w-[512px] mx-auto px-4 py-8">
+      <h1 className="text-2xl font-bold text-center">AutoView Playground</h1>
       <ol className="list-decimal list-inside text-xs mt-4">
         <li className="py-1">
           Edit <CodeBlock>src/env.ts</CodeBlock> to set your API key.
         </li>
         <li className="py-1">
-          Edit <CodeBlock>src/YourSchema.ts</CodeBlock> to define your own schema.
+          Edit <CodeBlock>src/YourSchema.ts</CodeBlock> to define your own
+          schema.
         </li>
         <li className="py-1">
           Run <CodeBlock>npm run generate</CodeBlock> to generate the component.
         </li>
         <li className="py-1">
-          Run <CodeBlock>npm run start</CodeBlock> to see the component in action.
+          Run <CodeBlock>npm run start</CodeBlock> to see the component in
+          action.
         </li>
       </ol>
-    <div className="mt-8">
-      <AutoViewComponent />
+      <div className="mt-8">
+        <AutoViewComponent />
+      </div>
     </div>
-  </div>
+  );
 }
 
 function CodeBlock({ children }: { children: React.ReactNode }) {
-  return <code className="bg-gray-200 px-1 py-0.5 rounded-md font-bold">{children}</code>
+  return (
+    <code className="bg-gray-200 px-1 py-0.5 rounded-md font-bold">
+      {children}
+    </code>
+  );
 }
 
 function AutoViewComponent() {
--- vite.config.ts
+++ vite.config.ts
@@ -1,9 +1,8 @@
 import typia from "@ryoppippi/unplugin-typia/vite";
-import tailwindcss from '@tailwindcss/vite'
 import react from "@vitejs/plugin-react";
 import { defineConfig } from "vite";
 
 // https://vite.dev/config/
 export default defineConfig({
-  plugins: [typia(), react(), tailwindcss()],
+  plugins: [typia(), react()],
 });
