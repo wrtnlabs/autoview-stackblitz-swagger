266acdc0172b7a7bd8e0f0c5a79342512ae4a9bc
Jeongho Nam <samchon.github@gmail.com>
2025-03-31 15:49:48 +0900

    Complete swagger playground

--- src/generate.ts
+++ src/generate.ts
@@ -1,45 +1,73 @@
 import { MainAgent } from "@autoview/agent";
+import {
+  HttpLlm,
+  IHttpLlmApplication,
+  IHttpLlmFunction,
+  OpenApi,
+} from "@samchon/openapi";
 import fs from "fs";
 import OpenAI from "openai";
-import { exit } from "process";
 import typia from "typia";
 
-import env from "./env";
-import { YourSchema } from "./YourSchema";
+import env from "./env.js";
+import { checkApiKey } from "./internal/checkApiKey.js";
 
 const main = async (): Promise<void> => {
   checkApiKey();
 
-  const result: MainAgent.IResult = await MainAgent.execute(
-    {
-      type: "chatgpt",
-      api: new OpenAI({
-        apiKey: typia.assert<string>(env.OPENAI_API_KEY),
-      }),
-      model: typia.assert<OpenAI.ChatModel>(env.OPENAI_MODEL),
-    },
-    typia.llm.parameters<YourSchema, "chatgpt">(),
+  // GET SWAGGER SCHEMA INFORMATION
+  const document: OpenApi.IDocument = OpenApi.convert(
+    await fetch("https://shopping-be.wrtn.ai/editor/swagger.json").then((r) =>
+      r.json(),
+    ),
   );
-  await fs.promises.writeFile(
-    "src/transform.ts",
-    result.transformTsCode,
-    "utf8",
+
+  // CONVERT TO LLM FUNCTION CALLING SCHEMA
+  const app: IHttpLlmApplication<"chatgpt"> = HttpLlm.application({
+    model: "chatgpt",
+    document,
+    options: {
+      reference: true,
+    },
+  });
+  const page: IHttpLlmFunction<"chatgpt"> | undefined = app.functions.find(
+    (func) =>
+      func.path === "/shoppings/customers/sales" && func.method === "patch",
+  );
+  const sale: IHttpLlmFunction<"chatgpt"> | undefined = app.functions.find(
+    (func) =>
+      func.path === "/shoppings/customers/sales/{id}" && func.method === "get",
+  );
+  if (page === undefined || sale === undefined) {
+    console.error("Operation not found");
+    process.exit(-1);
+  }
+
+  // GENERATE
+  await Promise.all(
+    Object.entries({
+      page,
+      sale,
+    }).map(async ([key, func]) => {
+      const result: MainAgent.IResult = await MainAgent.execute(
+        {
+          type: "chatgpt",
+          api: new OpenAI({
+            apiKey: typia.assert<string>(env.OPENAI_API_KEY),
+          }),
+          model: typia.assert<OpenAI.ChatModel>(env.OPENAI_MODEL),
+        },
+        {
+          ...func.output,
+          $defs: func.parameters.$defs,
+        },
+      );
+      await fs.promises.writeFile(
+        `src/transform${key[0].toUpperCase()}${key.slice(1)}.ts`,
+        result.transformTsCode,
+        "utf8",
+      );
+    }),
   );
 };
-
 main().catch(console.error);
-
-function checkApiKey(): void {
-  const apiKey = process.env.OPENAI_API_KEY;
-
-  if (!apiKey || apiKey === "YOUR_OPENAI_API_KEY") {
-    console.error("");
-    console.error("[========= API KEY ERROR =========]");
-    console.error("");
-    console.error("`OPENAI_API_KEY` is not set; please open the `src/env.ts` file and feed your own key");
-    console.error("");
-    console.error("[=================================]");
-    console.error("");
-    exit(-1);
-  }
-}
